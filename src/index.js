45035c644e72f17ca6a343b559abb45fec39388a
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2021-02-26 00:31:09 +0300

    feat: add implementation of towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix || matrix.length === 0) return [];
+    const res = [];
+    const n = matrix.length;
+    for (let i = 0; i < n; i++) {
+        if (i & 1) {
+            res.push(...matrix[i].reverse());
+        } else {
+            res.push(...matrix[i]);
+        }
+    }
+    return res;
+};
